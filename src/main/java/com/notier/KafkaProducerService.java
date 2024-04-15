37f61fc0b70fbaac8eb161bd5c7e909a0ba077e2
juyongc <chunjy2147@gmail.com>
2024-04-15 16:36:08 +0900

    [Feat] kafka producer config & test

---
+++ src/main/java/com/notier/KafkaProducerService.java
@@ -0,0 +1,17 @@
+package com.notier;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class KafkaProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+
+}
