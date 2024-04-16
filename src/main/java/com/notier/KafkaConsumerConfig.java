f52706dc0cc9f84a0b5f10fbd636ccdbad771941
juyongc <chunjy2147@gmail.com>
2024-04-16 15:04:18 +0900

    [Feat] kafka consumer config & test complete

---
+++ src/main/java/com/notier/KafkaConsumerConfig.java
@@ -0,0 +1,36 @@
+package com.notier;
+
+import java.util.HashMap;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+
+        HashMap<String, Object> consumerProps = new HashMap<>();
+
+        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:10000");
+        consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "error-handler-group");
+        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return new DefaultKafkaConsumerFactory<>(consumerProps);
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(ConsumerFactory<String, String> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+
+}
--- src/main/java/com/notier/rateService/HelloService.java
+++ src/main/java/com/notier/rateService/HelloService.java
@@ -4,24 +4,26 @@ import com.notier.entity.MemberEntity;
 import com.notier.repository.MemberRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class RateService {
+public class HelloService {
 
     private final MemberRepository memberRepository;
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public void sendKafkaMessage() {
+    public void sendKafkaTestMessage() {
         List<MemberEntity> memberList = memberRepository.findAll();
         for (MemberEntity memberEntity : memberList) {
             kafkaTemplate.send("error-messages", memberEntity.getName() + " Hello!!!");
         }
     }
-
-    public void sendRateUpDownAlarm() {
+    @KafkaListener(id = "error-listen", topics = "error-messages")
+    public void listenKafkaMessage(String message) {
+        System.out.println("consumer get message : " + message);
     }
 
 }
