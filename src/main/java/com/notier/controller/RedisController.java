e49bf3c5269ff7bdf92072b1d1da213c1c69d2df
juyongc <chunjy2147@gmail.com>
2024-06-15 20:17:47 +0900

    [Feat] redis 환경세팅 및 테스트

---
+++ src/main/java/com/notier/controller/RedisController.java
@@ -0,0 +1,30 @@
+package com.notier.controller;
+
+import com.notier.rateService.RedisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/redis")
+public class RedisController {
+
+    private final RedisService redisService;
+
+    @PostMapping("/save")
+    public String saveData(@RequestParam String key, @RequestParam Object value) {
+        redisService.saveData(key, value);
+        return "Save Success~~~!!";
+    }
+
+    @GetMapping("/get")
+    public Object getData(@RequestParam String key) {
+        return redisService.getData(key);
+    }
+
+
+}
