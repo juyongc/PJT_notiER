1bf09ac7897632da437bf2e7f7e1e81d2f438a42
juyongc <chunjy2147@gmail.com>
2024-06-15 20:17:47 +0900

    [Feat] redis 환경세팅 및 테스트

---
+++ src/main/java/com/notier/rateService/RedisService.java
@@ -0,0 +1,21 @@
+package com.notier.rateService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void saveData(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public Object getData(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+}
