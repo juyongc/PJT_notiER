1c1b501752d48f91a31ab8a5381f1d389724437e
juyongc <chunjy2147@gmail.com>
2024-08-22 11:10:43 +0900

    [feat] USD, JPY 외 다른 티커에 대한 알람 생성 테스트 코드 추가

--- src/test/java/com/notier/InsertAlarmTest.java
+++ src/test/java/com/notier/InsertAlarmTest.java
@@ -7,8 +7,12 @@ import com.notier.rateService.RedisService;
 import com.notier.repository.AlarmRepository;
 import com.notier.repository.CurrencyRepository;
 import com.notier.repository.MemberRepository;
+import java.util.Collections;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +33,10 @@ class InsertAlarmTest {
     @Autowired
     private RedisService redisService;
 
+    /**
+     * USD, JPY 알람 엔티티 생성
+     * 로직 : 모든 유저 알람 추가
+     */
     @Test
     void insertAlarmSettingCode() {
 
@@ -42,8 +50,8 @@ class InsertAlarmTest {
             MemberEntity memberEntity = memberRepository.findById(i)
                 .orElseThrow(() -> new NoSuchElementException("없는 고객 ID입니당"));
 
-            Long jpyWishRate = jpy.getExchangeRate() + random.nextInt(41) - 10;
-            Long usdWishRate = usd.getExchangeRate() + random.nextInt(41) - 10;
+            Long jpyWishRate = jpy.getExchangeRate() + random.nextInt(41) - 20;
+            Long usdWishRate = usd.getExchangeRate() + random.nextInt(41) - 20;
 
             AlarmEntity jpyAE = AlarmEntity.builder()
                 .currencyEntity(jpy)
@@ -59,6 +67,47 @@ class InsertAlarmTest {
 
             alarmRepository.save(jpyAE);
             alarmRepository.save(usdAE);
+        }
+
+    }
+
+    /**
+     * USD, JPY 제외 티커들 알람 엔티티 생성
+     * 로직 : 각 티커별 랜덤 100명씩만 추가
+     */
+    @Test
+    void insertAlarmInAllTickerExceptUsdJpy() {
+
+        List<Long> numbers = LongStream.range(5, 1600)
+            .boxed().collect(Collectors.toList());
+
+        List<CurrencyEntity> currencyEntities = currencyRepository.findAll();
+
+        for (CurrencyEntity currencyEntity : currencyEntities) {
+            String ticker = currencyEntity.getTicker();
+            if (ticker.equals("USD") || ticker.equals("JPY")) {
+                continue;
+            }
+
+            Collections.shuffle(numbers, new Random());
+
+            List<Long> randomNumbers = numbers.subList(0, 100);
+
+            for (Long randomNumber : randomNumbers) {
+                MemberEntity memberEntity = memberRepository.findById(randomNumber)
+                    .orElseThrow(() -> new NoSuchElementException("없는 고객 ID입니당"));
+
+                Long wishRate = currencyEntity.getExchangeRate() + random.nextInt(41) - 20;
+
+                AlarmEntity ae = AlarmEntity.builder()
+                    .currencyEntity(currencyEntity)
+                    .memberEntity(memberEntity)
+                    .wishRate(wishRate)
+                    .build();
+
+                alarmRepository.save(ae);
+            }
+
         }
 
     }
